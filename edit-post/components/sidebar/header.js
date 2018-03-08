a78d5ea4ec2c2d35d49b7573a3fbaf58b682c5bf
Robert Anderson <robert@noisysocks.com>
2018-03-08 11:49:25 -0800

    Improve settings UI on mobile devices
    On mobile devices, display the post title in a panel header above the
    settings UI.

--- edit-post/components/sidebar/header.js
+++ edit-post/components/sidebar/header.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 /**
  * WordPress dependencies
  */
-import { compose } from '@wordpress/element';
+import { Fragment, compose } from '@wordpress/element';
 import { __, _n, sprintf } from '@wordpress/i18n';
 import { IconButton } from '@wordpress/components';
 import { withSelect } from '@wordpress/data';
@@ -17,37 +17,50 @@ import { withSelect } from '@wordpress/data';
 import { getActiveEditorPanel } from '../../store/selectors';
 import { closeGeneralSidebar, setGeneralSidebarActivePanel } from '../../store/actions';
 
-const SidebarHeader = ( { panel, onSetPanel, onCloseSidebar, count } ) => {
+const SidebarHeader = ( { title, count, panel, onSetPanel, onCloseSidebar } ) => {
 	// Do not display "0 Blocks".
 	count = count === 0 ? 1 : count;
 
 	return (
-		<div className="components-panel__header edit-post-sidebar__panel-tabs">
-			<button
-				onClick={ () => onSetPanel( 'document' ) }
-				className={ `edit-post-sidebar__panel-tab ${ panel === 'document' ? 'is-active' : '' }` }
-				aria-label={ __( 'Document settings' ) }
-			>
-				{ __( 'Document' ) }
-			</button>
-			<button
-				onClick={ () => onSetPanel( 'block' ) }
-				className={ `edit-post-sidebar__panel-tab ${ panel === 'block' ? 'is-active' : '' }` }
-				aria-label={ __( 'Block settings' ) }
-			>
-				{ sprintf( _n( 'Block', '%d Blocks', count ), count ) }
-			</button>
-			<IconButton
-				onClick={ onCloseSidebar }
-				icon="no-alt"
-				label={ __( 'Close settings' ) }
-			/>
-		</div>
+		<Fragment>
+			<div className="components-panel__header edit-post-sidebar__header">
+				<span className="edit-post-sidebar__title">
+					{ title }
+				</span>
+				<IconButton
+					onClick={ onCloseSidebar }
+					icon="no-alt"
+					label={ __( 'Close settings' ) }
+				/>
+			</div>
+			<div className="components-panel__header edit-post-sidebar__panel-tabs">
+				<button
+					onClick={ () => onSetPanel( 'document' ) }
+					className={ `edit-post-sidebar__panel-tab ${ panel === 'document' ? 'is-active' : '' }` }
+					aria-label={ __( 'Document settings' ) }
+				>
+					{ __( 'Document' ) }
+				</button>
+				<button
+					onClick={ () => onSetPanel( 'block' ) }
+					className={ `edit-post-sidebar__panel-tab ${ panel === 'block' ? 'is-active' : '' }` }
+					aria-label={ __( 'Block settings' ) }
+				>
+					{ sprintf( _n( 'Block', '%d Blocks', count ), count ) }
+				</button>
+				<IconButton
+					onClick={ onCloseSidebar }
+					icon="no-alt"
+					label={ __( 'Close settings' ) }
+				/>
+			</div>
+		</Fragment>
 	);
 };
 
 export default compose(
 	withSelect( ( select ) => ( {
+		title: select( 'core/editor' ).getEditedPostAttribute( 'title' ),
 		count: select( 'core/editor' ).getSelectedBlockCount(),
 	} ) ),
 	connect(
